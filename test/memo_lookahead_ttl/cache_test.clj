5c82733d08b06b9f6051ae4cf69988f8b6717d9e
LudoTheHUN <ludoart@yahoo.com>
2014-08-30 15:33:09 +0100

    composition tests

--- test/memo_lookahead_ttl/cache_test.clj
+++ test/memo_lookahead_ttl/cache_test.clj
@@ -69,3 +69,73 @@
       (is (=  (second (second (second (first posthitcahe)))) 20))
       ; but is still serving the value from the 20's loop since it has not :ttlookup expired yet.
       (is (=  (cca/lookup posthitcahe :c) 20))))
+
+
+
+  (deftest cache-compositon
+
+    (def combined_cache1 (-> {:a 2 :b 3 :c 4}
+                             (cca/fifo-cache-factory :threshold 2)
+                             (ttl-lookahead-cache-factory :ttl 1000 :ttlookup 1500)
+                             ))
+
+
+    (def combined_cache2 (-> {:a 2 :b 3 :c 4}
+                             (ttl-lookahead-cache-factory :ttl 1000 :ttlookup 1500)
+                             (cca/fifo-cache-factory :threshold 2)
+                             ))
+
+
+    (cca/lookup combined_cache1 :a)
+    (cca/lookup combined_cache2 :a)
+
+    (cca/lookup combined_cache1 :b)
+    (cca/lookup combined_cache2 :b)
+
+    (cca/miss combined_cache1 :d 5)
+    (cca/miss combined_cache2 :d 5)
+
+    (cca/miss (cca/miss combined_cache1 :d 5) :d 6)
+    (cca/miss (cca/miss combined_cache2 :d 5) :d 6)
+
+
+    (cca/miss (cca/miss combined_cache1 :d 5) :e 6)
+    (cca/miss (cca/miss combined_cache2 :d 5) :e 6)
+
+
+    ;; fifi behaviour is effectively overridden by ttl-lookahead, so this composition is fruitless
+    (is (= (count  (cca/miss (cca/miss (cca/miss combined_cache1 :d 5) :e 6) :f 7)) 5))  ;;Note this will only be true until :ttlookup timeout
+
+
+    ;; fifi behaviour is maintained while we also have ttl-lookahead behaviour
+    (let [ca (cca/miss (cca/miss (cca/miss combined_cache2 :d 5) :e 6) :f 7)]
+             (is (= (count ca) 2))    ;; fifo has limited us to 2
+            (Thread/sleep 2000)
+             (is (= (count (cca/miss ca :q 9))) 1)   )  ;;TTL has removed value
+
+
+
+
+    (def combined_cache_a1  (-> {:a 2 :b 3 :c 4} (cca/fifo-cache-factory :threshold 2)))
+    (def combined_cache_b1  (ttl-lookahead-cache-factory combined_cache_a1 :ttl 2000 :ttlookup 3000))
+
+    (def combined_cache_a2  (-> {:a 2 :b 3 :c 4} (ttl-lookahead-cache-factory :ttl 2000 :ttlookup 3000)))
+    (def combined_cache_b2  (cca/fifo-cache-factory combined_cache_a2 :threshold 2))
+
+
+
+    (cca/lookup combined_cache_b1 :a)
+    combined_cache_a1
+    combined_cache_b1
+
+    (cca/lookup combined_cache_b2 :a)
+    combined_cache_a2
+    combined_cache_b2
+
+  ;;; Question... is combined_cache_b1 going to become large as we hit it (defeating the fifo it has been composed with?)
+  ;;; A: Yes, combined_cache_b1 will keep growing (as far as :ttl lets it be) , but combined_cache_b2 will always be size 2 capped
+
+    )
+
+
+
